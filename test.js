7b1c911ba4509087af25933ff9331aa863a29a69
wf <feng.wang@careerdream.org>
2016-09-19 16:15:44 +0800

    testjs

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-﻿alert(888);
+﻿//alert(888"");
 
 var divNode=document.createElement('div');
 var textNode=document.createTextNode('牛逼');
 divNode.appendChild(textNode);
-document.body.innerHTML="宝箱岭、隔壁老陆";
+document.body.innerHTML="宝箱岭、隔壁老陆——————老王到此一游";
